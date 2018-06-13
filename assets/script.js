1805e34414e7ade2130871bce03400ccf7227789
Igor Chernyshev <igorchern07@gmail.com>
2018-06-13 16:03:00 -0500

    working with placeholder spaces

--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,78 @@
 const canvas = document.querySelector('canvas');
 const message = document.querySelector('.message');
+const button = document.querySelector('button');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-let mistakes = 0;
-
 let blank = 'transparent';
-
 const c = canvas.getContext('2d');
 
 //gallows
 c.beginPath();
 c.lineWidth = 10;
-c.moveTo(750, 600);
-c.lineTo(1160, 600);
-c.moveTo(850, 600);
-c.lineTo(850, 100);
-c.lineTo(1100, 100);
-c.lineTo(1100, 180);
-c.moveTo(905, 100);
-c.lineTo(850, 160);
+c.moveTo(0, 600);
+c.lineTo(410, 600);
+c.moveTo(100, 600);
+c.lineTo(100, 100);
+c.lineTo(350, 100);
+c.lineTo(350, 180);
+c.moveTo(155, 100);
+c.lineTo(100, 160);
 c.stroke();
+
 function drawMan() {
   //head
   c.beginPath();
-  c.arc(1100, 220, 40, 0, 2 * Math.PI, true);
+  c.arc(350, 220, 40, 0, 2 * Math.PI, true);
   c.strokeStyle = mistakes === 1 ? '#ff0000' : mistakes > 1 ? '#000' : blank;
   c.stroke();
   //torso
   c.beginPath();
-  c.moveTo(1100, 265);
-  c.lineTo(1100, 350);
+  c.moveTo(350, 265);
+  c.lineTo(350, 350);
   c.strokeStyle = mistakes === 2 ? '#ff0000' : mistakes > 2 ? '#000' : blank;
   c.stroke();
   //right leg
   c.beginPath();
-  c.moveTo(1100, 348);
-  c.lineTo(1150, 415);
+  c.moveTo(350, 348);
+  c.lineTo(400, 415);
   c.strokeStyle = mistakes === 3 ? '#ff0000' : mistakes > 3 ? '#000' : blank;
   c.stroke();
   //left leg
   c.beginPath();
-  c.moveTo(1100, 348);
-  c.lineTo(1050, 415);
+  c.moveTo(350, 348);
+  c.lineTo(300, 415);
   c.strokeStyle = mistakes === 4 ? '#ff0000' : mistakes > 4 ? '#000' : blank;
   c.stroke();
   //right arm
   c.beginPath();
-  c.moveTo(1104, 300);
-  c.lineTo(1155, 290);
+  c.moveTo(354, 300);
+  c.lineTo(405, 290);
   c.strokeStyle = mistakes === 5 ? '#ff0000' : mistakes > 5 ? '#000' : blank;
   c.stroke();
   //left arm
   c.beginPath();
-  c.moveTo(1096, 300);
-  c.lineTo(1045, 290);
+  c.moveTo(346, 300);
+  c.lineTo(295, 290);
   if (mistakes === 6) {
     c.strokeStyle = '#ff0000';
     c.stroke();
-    setTimeout(function () {
-      c.moveTo(1100, 300);
-      c.lineTo(1050, 290);
+    setTimeout(function() {
+      c.moveTo(346, 300);
+      c.lineTo(300, 290);
       c.strokeStyle = '#000';
       c.stroke();
       answerText.innerHTML = correctAnswer;
       message.classList.add('lose');
+      button.classList.add('show');
+      document.onkeyup = function() {
+        reset();
+      };
     }, 1000);
   } else {
     c.strokeStyle = '#fff';
     c.stroke();
   }
 }
-
-drawMan();
 
 const url = 'https://opentdb.com/api.php?amount=1&category=12&type=multiple';
 const question = document.querySelector('#question');
@@ -84,16 +84,17 @@ function getData() {
   axios
     .get(url)
     .then(res => {
-      const letters = /^[A-Za-z]+$/;
+      const letters = /^[a-zA-Z ]+$/;
       let answer = res.data.results[0].correct_answer;
-      if (!(answer.match(letters))) {
-        getData();
+      console.log('answer: ', answer);
+      if (!answer.match(letters)) {
+        reset();
       }
       let answerArr = answer.toLowerCase().split('');
       if (answer.length >= 6) {
         correctAnswer = res.data.results[0].correct_answer;
       } else {
-        getData();
+        reset();
       }
       question.innerHTML = res.data.results[0].question;
       let placeholder = answer
@@ -102,9 +103,22 @@ function getData() {
           return letter.replace(letter, '_');
         })
         .join('');
-      answerText.innerHTML = placeholder;
       let placeholderArr = placeholder.split('');
-      document.onkeyup = function (event) {
+      if (answer.indexOf(' ') !== -1) {
+        spaceIndexArr = [];
+        for (let i = 0; i < placeholderArr.length; i++) {
+          if (placeholderArr[i] === ' ') {
+            spaceIndexArr.push(i);
+          }
+        }
+        spaceIndexArr.forEach(index => {
+          placeholderArr.splice(index, 1, ' ');
+        });
+      }
+
+      answerText.innerHTML = placeholder;
+
+      document.onkeyup = function(event) {
         if (event.keyCode === 13) {
           reset();
           return;
@@ -117,41 +131,41 @@ function getData() {
               indexArr.push(i);
             }
           }
-          console.log('indexArr: ', indexArr);
-          console.log('guess: ', guess);
-          console.log('p1 array: ', placeholder.split(''));
 
-          indexArr.forEach(number => {
-            placeholderArr.splice(number, 1, guess);
+          indexArr.forEach(index => {
+            placeholderArr.splice(index, 1, guess);
           });
 
-          console.log(
-            'p2: ',
-            placeholderArr.splice(0, 1, placeholderArr[0].toUpperCase())
-          );
           placeholder = placeholderArr.join('');
-          console.log('the real placeholder: ', placeholder);
+
+          placeholderArr.splice(0, 1, placeholderArr[0].toUpperCase());
+
+          placeholder = placeholderArr.join('');
           answerText.innerHTML = placeholder;
           if (placeholder.indexOf('_') === -1) {
             message.classList.add('win');
+            button.classList.add('show');
+            document.onkeyup = function() {
+              reset();
+            };
           }
         } else {
           mistakes++;
           drawMan();
         }
-        console.log('answer: ', answer);
       };
     })
     .catch(error => {
       console.log(error);
     });
 }
-getData();
+reset();
 next.addEventListener('click', () => {
   reset();
 });
 
 function reset() {
+  button.classList.remove('show');
   answerText.innerHTML = '';
   mistakes = 0;
   blank = '#fff';
@@ -163,9 +177,7 @@ function reset() {
   getData();
 }
 
-// const arr = [1, 2, 3, 4, 2, 3, 4, 7, 5, 8, 9];
-
-// arr.reduce((indexArr, num, index) => {
-//   if (num === 2) indexArr.push(index);
+// arr.reduce((indexArr, val, index) => {
+//   if (val === guess) indexArr.push(index);
 //   return indexArr;
 // }, []);
